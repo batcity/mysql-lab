70293ba332df96ae6ee30e10a032a93095aefd69
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-10-17 00:10:46 +0530

    get started on MYSQL Isolation example

---
+++ isolation_levels/IsolationExample.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+public class IsolationExample {
+
+    public static void main(String args[]) {
+        createTestTable();
+    }
+
+
+    private static boolean createTestTable() {
+
+        return true;
+    }
+}
+
+
+
