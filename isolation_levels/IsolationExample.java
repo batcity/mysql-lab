7cdc1fc3cee0839991c749c1c8e9f5364bb0e513
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-11-07 01:01:06 +0530

    add example showing transaction isolation level

--- isolation_levels/IsolationExample.java
+++ isolation_levels/IsolationExample.java
@@ -2,14 +2,35 @@ import java.sql.*;
 
 public class IsolationExample {
 
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/labdb?allowLoadLocalInfile=true";
+    private static final String ROOT_USER = "root";
+    private static final String ROOT_PASS = "root";
+
     public static void main(String args[]) {
-        createTestTable();
-    }
+        try {
+            Connection writer = DriverManager.getConnection(DB_URL, ROOT_USER, ROOT_PASS);
+            writer.setAutoCommit(false);
+            writer.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED);
+            Statement stmt = writer.createStatement();
 
+            stmt.execute("Insert into employees values(50, 'Zeus', 'god', 10000)");
+            ResultSet results = stmt.executeQuery("SELECT * FROM employees");
+            System.out.println("read uncommitted isolation level picks up uncommitted changes");
+            while (results.next()) {
+                System.out.println(results.getString("name"));
+            }
 
-    private static boolean createTestTable() {
-
-        return true;
+            Connection reader = DriverManager.getConnection(DB_URL, ROOT_USER, ROOT_PASS);
+            reader.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+            Statement stmtTwo = reader.createStatement();
+            System.out.println("This block doesn't print anything since the transaction wasn't committed");
+            ResultSet resultsTwo = stmtTwo.executeQuery("SELECT * FROM employees");
+            while (resultsTwo.next()) {
+                System.out.println(resultsTwo.getString("name"));
+            }
+        } catch(Exception exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
 
