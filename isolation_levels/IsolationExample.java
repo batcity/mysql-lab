0daa1f21ba71dfdc4740e3cc5a9c9ec1f1abae07
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-12-19 01:35:25 +0530

    refine isolation example

--- isolation_levels/IsolationExample.java
+++ isolation_levels/IsolationExample.java
@@ -35,7 +35,7 @@ public class IsolationExample {
             Connection reader = DriverManager.getConnection(DB_URL, ROOT_USER, ROOT_PASS);
             reader.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
             Statement stmtTwo = reader.createStatement();
-            System.out.println("This block doesn't print anything since the transaction wasn't committed");
+            System.out.println("read committed block here doesn't print anything since the previous write wasn't committed");
             ResultSet resultsTwo = stmtTwo.executeQuery("SELECT * FROM employees");
             while (resultsTwo.next()) {
                 System.out.println(resultsTwo.getString("name"));
@@ -50,19 +50,32 @@ public class IsolationExample {
             System.out.println("This repeatable block should return the same data twice even though there's an"
             + " additional insertion between the two reads");
             ResultSet repeatableResult = repeatableStmt.executeQuery("SELECT * FROM employees");
+            System.out.println("Here's the first read:");
             while (repeatableResult.next()) {
                 System.out.println(repeatableResult.getString("name"));
             }
 
+            System.out.println("Here's the write that won't get picked up...");
             stmt.execute("Insert into employees values(51, 'Apollo', 'god', 10000)");
             writer.commit();
 
             ResultSet repeatableResultPartTwo = repeatableStmt.executeQuery("SELECT * FROM employees");
+            System.out.println("Here's the second read:");
             while (repeatableResultPartTwo.next()) {
                 System.out.println(repeatableResultPartTwo.getString("name"));
             }
 
-            writer.rollback();
+            // Step-by-Step Demo Plan for serializable writes
+            // 1️⃣ Serializable reader
+            // Starts transaction
+            // Does SELECT
+            // Never commits
+            // 2️⃣ Writer
+            // Attempts INSERT
+            // Blocks
+            // Eventually times out
+            // Throws exception
+            
         } catch(Exception exception) {
             System.out.println(exception.getMessage());
         }
